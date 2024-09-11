7d87e0945eda5131d173a69df4f4ffad389bcfa6
mariyamango <de.msomkova@gmail.com>
2024-09-11 15:26:08 +0200

    Added command-line management tool

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,65 +1,140 @@
 package org.example;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final ProductRepo productRepo = new ProductRepo();
+    private static final OrderRepo orderRepo = new OrderListRepo();
+    private static final ShopService shopService = new ShopService(productRepo, orderRepo);
+
     public static void main(String[] args) {
 
-        System.out.println("Create Product, ProductRepo, manipulations with ProductRepo:");
-        Product milk = new Product(1, "Milk", new BigDecimal("0.99"),"Milk produced by MilkCompany");
-        Product bread = new Product(2, "Bread", new BigDecimal("1.19"),"Bread produced by BreadCompany");
-        Product cheese = new Product(3, "Cheese", new BigDecimal("0.89"),"Cheese produced by CheeseCompany");
-        Product cake = new Product(4, "Cake", new BigDecimal("2.99"),"Cake produced by CakeCompany");
-        Product sausage = new Product(5, "Sausage", new BigDecimal("1.24"),"Sausage produced by SausageCompany");
-        Product eggs = new Product(6, "Eggs", new BigDecimal("1.95"),"10 eggs produced by EggCompany");
-        System.out.println(milk);
-        ProductRepo repo = new ProductRepo(List.of(milk, bread, cheese, cake, sausage));
-        System.out.println(repo);
-        System.out.println(repo.getProductById(4));
-        repo.removeProduct(3);
-        System.out.println("After removing product with id 3: ");
-        repo.getProductList().forEach(System.out::println);
-        repo.addProduct(cheese);
-        System.out.println("After adding product with id 3: ");
-        repo.getProductList().forEach(System.out::println);
-        System.out.println("--------------------------------------------------------------------");
-        System.out.println();
+        boolean on = true;
 
-        System.out.println("Manipulations with Map and List and Interface:");
-        OrderRepo orderMapRepo = new OrderMapRepo();
-        OrderRepo orderListRepo = new OrderListRepo();
+        while (on) {
+            printMenu();
+            String option = scanner.nextLine();
+            switch (option) {
+                case "1" -> createNewProduct();
+                case "2" -> removeProduct();
+                case "3" -> printProducts();
+                case "4" -> createNewOrder();
+                case "5" -> printOrders();
+                case "6" -> changeQuantityInProductOfOrder();
+                case "7" -> on = false;
+                default -> System.out.println("Incorrect option! Please try again.");
+            }
+        }
+        scanner.close();
+    }
 
-        ShopService shopServiceMap = new ShopService(repo, orderMapRepo);
-        ShopService shopServiceList = new ShopService(repo, orderListRepo);
+    private static void printMenu() {
+        System.out.println("\nWelcome to Command-line Product Management Tool! Choose an option:");
+        System.out.println("1. Add a product");
+        System.out.println("2. Remove a product");
+        System.out.println("3. Print a list of all the products");
+        System.out.println("4. Place an order");
+        System.out.println("5. Print a list of all the orders");
+        System.out.println("6. Modify a quantity of product in an order");
+        System.out.println("7. Exit");
+        System.out.print("Your option: ");
+    }
 
-        Map<Product,Integer> productMapOrder1 = new HashMap<>();
-        productMapOrder1.put(milk, 2);
-        productMapOrder1.put(bread, 3);
-        Order order1 = new Order (1, productMapOrder1);
-        Map<Product,Integer> productMapOrder2 = new HashMap<>();
-        productMapOrder2.put(cheese, 4);
-        productMapOrder2.put(sausage, 8);
-        Order order2 = new Order(2, productMapOrder2);
-        Map<Product,Integer> productMapOrder3 = new HashMap<>();
-        productMapOrder3.put(milk, 3);
-        productMapOrder3.put(cake, 1);
-        productMapOrder3.put(sausage, 2);
-        Order order3 = new Order(3, productMapOrder3);
+    private static void createNewProduct() {
+        System.out.print("Enter a new product identifier: ");
+        int id = Integer.parseInt(scanner.nextLine());
 
-        shopServiceMap.placeNewOrder(order1);
-        shopServiceMap.placeNewOrder(order3);
-        System.out.println("Orders in shopServiceMap:\n" + shopServiceMap);
-        shopServiceList.placeNewOrder(order2);
-        shopServiceList.placeNewOrder(order3);
-        System.out.println("Orders in shopServiceList:\n" + shopServiceList);
-        System.out.println("--------------------------------------------------------------------");
-        System.out.println();
+        System.out.print("Enter a new product name: ");
+        String name = scanner.nextLine();
 
-        System.out.println("Manipulations with prices and quantity:");
-        System.out.println("Total price of order number " + order1.getId() + " is: " + order1.getTotalPrice());
-        shopServiceMap.updateProductQuantity(1, milk, 1);
+        System.out.print("Enter a new product price: ");
+        BigDecimal price = BigDecimal.valueOf(Double.parseDouble(scanner.nextLine()));
+
+        System.out.print("Enter a new product description: ");
+        String description = scanner.nextLine();
+
+        Product product = new Product(id, name, price, description);
+        productRepo.addProduct(product);
+
+        System.out.println("Product added successfully.");
+    }
+
+    private static void removeProduct() {
+        System.out.print("Enter a product identifier to remove: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        if (productRepo.getProductById(id) == null) {
+            System.out.println("Product not found.");
+            return;
+        }
+        productRepo.removeProduct(id);
+        System.out.println("Product is removed successfully.");
+    }
+
+    private static void printProducts() {
+        List<Product> products = productRepo.getProductList();
+        if (products.isEmpty()) {
+            System.out.println("No products in the list.");
+        } else {
+            System.out.println("Available Products:");
+            products.forEach(product -> System.out.println(product.id() + ": \t" + product.name() + "\t - " + product.price() + "\t - " + product.description()));
+        }
+    }
+
+    private static void createNewOrder() {
+        System.out.print("Enter a new order identifier: ");
+        int orderId = Integer.parseInt(scanner.nextLine());
+        Map<Product, Integer> orderProducts = new HashMap<>();
+        while (true) {
+            System.out.print("Enter product identifier to add to a new order. Or type \"done\" to finish: ");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("done")) {
+                break;
+            }
+            int productId = Integer.parseInt(input);
+            Product productOpt = productRepo.getProductById(productId);
+            if (productOpt != null) {
+                System.out.print("Enter quantity for " + productOpt.name() + ": ");
+                int quantity = Integer.parseInt(scanner.nextLine());
+                orderProducts.put(productOpt, quantity);
+            } else {
+                System.out.println("Product not found.");
+            }
+        }
+        Order order = new Order(orderId, orderProducts);
+        shopService.placeNewOrder(order);
+    }
+
+    private static void printOrders() {
+        List<Order> orders = orderRepo.getOrdertList();
+        if (orders.isEmpty()) {
+            System.out.println("No orders in the list.");
+        } else {
+            orders.forEach(order -> {
+                System.out.println("Order id: " + order.getId());
+                System.out.println("Products:");
+                order.getProductQuantity().forEach((product, quantity) ->
+                        System.out.println("\t" + product.name() + "\t x" + quantity + "\t - " + product.price()));
+                System.out.println("Total Price: \t" + order.getTotalPrice());
+            });
+        }
+    }
+
+    private static void changeQuantityInProductOfOrder() {
+        System.out.print("Enter an order identifier to modify: ");
+        int orderId = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter product identifier to modify its quantity: ");
+        int productId = Integer.parseInt(scanner.nextLine());
+
+        Product productOpt = productRepo.getProductById(productId);
+        if (productOpt != null) {
+            System.out.print("Enter new quantity: ");
+            int newQuantity = Integer.parseInt(scanner.nextLine());
+            shopService.updateProductQuantity(orderId, productOpt, newQuantity);
+        } else {
+            System.out.println("Product is not found.");
+        }
     }
 }
